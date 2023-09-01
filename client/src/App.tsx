055dc6f2ff7eb61a62075dfabdb7a7a3923c546e
rlaxotn2300 <rlaxotn2300@naver.com>
2023-09-01 15:19:08 +0900

    메인 사이드바 작업중

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
-
+import MainSidebar from "./components/Main";
 function App() {
   return (
     <div className="App">
       <header></header>
       <main>Page가 다 들어감</main>
+      <MainSidebar />
       <footer></footer>
     </div>
   );
