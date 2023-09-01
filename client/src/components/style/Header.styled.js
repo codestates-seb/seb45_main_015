f1d2473f5c86b8bec82dfe0f9361ef79a6f30e85
dice0314 <dice0314@naver.com>
2023-09-01 20:01:06 +0900

    feat: header 드롭다운 제작

--- client/src/components/style/Header.styled.js
+++ client/src/components/style/Header.styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import globalTokens from "../../design_tokens/global.json";
 
 export const Container = styled.header`
   display: flex;
@@ -21,7 +22,7 @@ export const LogoWrapper = styled.div`
   width: 237px;
   height: 48px;
   padding-right: 42px;
-  border-right: solid 1px #e6e8ec;
+  border-right: solid 1px ${globalTokens.heart.default.value};
 `;
 
 export const Logo = styled.img`
@@ -35,25 +36,32 @@ export const NavWrapper = styled.div`
   margin-left: 42px;
 `;
 
-export const NavButton = styled.button`
+export const Button = styled.button`
+  cursor: pointer;
+  padding: 0;
   border-style: none;
-  border-radius: 12px;
   background-color: #fff;
   font-size: 18px;
   color: #777e90;
   font-family: "Gmarket Sans";
   white-space: nowrap;
-  padding: 16px;
+  transition: 0.5s ease-out;
+  padding: 12px;
 
   &:hover {
-    background-color: #f0f0f0;
+    transition: 0.5s ease-out;
+    color: ${globalTokens.Button.default.value};
+  }
+
+  &.Icon {
+    font-size: 28px;
+    margin-left: 10px;
   }
 `;
 
 export const SearchWrapper = styled.div`
   width: 100%;
-  margin-right: 16px;
-  padding-right: 15px;
+  padding: 0 15px 0 15px;
 `;
 
 export const SearchForm = styled.div`
@@ -63,7 +71,7 @@ export const SearchForm = styled.div`
   height: 100%;
   border: solid 1px #777e90;
   border-radius: 45px;
-  padding-right: 10px;
+  padding-right: 15px;
 `;
 
 export const Search = styled.input`
@@ -71,7 +79,7 @@ export const Search = styled.input`
   border-style: none;
   background-color: transparent;
   width: 100%;
-  margin: 15px;
+  padding: 15px 5px 15px 15px;
   font-size: 16px;
 `;
 
@@ -88,19 +96,5 @@ export const SearchButton = styled.button`
 
 export const UserWrapper = styled.div`
   display: flex;
-`;
-
-export const UserButton = styled.button`
-  border-style: none;
-  border-radius: 12px;
-  background-color: #fff;
-  font-size: 18px;
-  color: #777e90;
-  font-family: "Gmarket Sans";
-  white-space: nowrap;
-  padding: 16px;
-
-  &:hover {
-    background-color: #f0f0f0;
-  }
+  margin-left: 15px;
 `;
---
+++ client/src/components/style/HeaderDropDown.styled.js
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+import globalTokens from "../../design_tokens/global.json";
+
+export const DropDown = styled.header`
+  position: absolute;
+  right: 410px;
+  margin-top: 5px;
+`;
+
+export const Decoration = styled.div`
+  position: absolute;
+  background-color: #fff;
+  margin-left: 210px;
+  border-radius: 6px;
+  width: 30px;
+  height: 30px;
+  rotate: -45deg;
+`;
+
+export const Container = styled.div`
+  position: absolute;
+  background-color: #fff;
+  width: 256px;
+  border-radius: 12px;
+  margin-top: 12px;
+  box-shadow: 0px 16px 64px rgba(31, 47, 70, 0.4);
+  padding: 32px 16px 16px 16px;
+`;
+
+export const UserName = styled.h2`
+  display: flex;
+  font-size: 24px;
+  margin: 0;
+  margin-bottom: 8px;
+  font-weight: bold;
+  color: #23262f;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 16px 0;
+  color: #23262f;
+
+  &:not(:first-of-type) {
+    border-top: solid 1px #e6e8ec;
+  }
+`;
+
+export const Icon = styled.div`
+  font-size: 18px;
+  color: #777e90;
+  margin-right: 10px;
+`;
+
+export const Text = styled.p`
+  margin: 0;
+  padding: 0;
+  font-size: 18px;
+  font-weight: bold;
+  color: #777e90;
+`;
