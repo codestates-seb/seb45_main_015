7dc96ad0ffc0e50b3060e58a0c7f78b35e67eb62
Dr-eong <rwg3465@gmail.com>
2023-09-01 12:04:32 +0900

    FE 초기세팅

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header></header>
+      <main>Page가 다 들어감</main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
