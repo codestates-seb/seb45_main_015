6e5b76927f8b89a57f3f3f20fa1e1cf1989fa8a1
rlawogks <otfine@gmail.com>
2023-09-04 19:11:36 +0900

    feat: 멤버 정보 불러오기 추가

--- server/src/main/java/com/project15/server/member/SecurityConfig.java
+++ server/src/main/java/com/project15/server/member/SecurityConfig.java
@@ -8,6 +8,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 
 @Configuration
@@ -39,6 +44,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(customMemberDetailsService)
                 .passwordEncoder(passwordEncoder());
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(""));
+        configuration.setAllowedMethods(Arrays.asList(""));
+        configuration.setAllowedHeaders(Arrays.asList(""));
+        configuration.setExposedHeaders(Arrays.asList(""));
+        configuration.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
---
+++ server/src/main/java/com/project15/server/member/WebConfig.java
@@ -0,0 +1,18 @@
+package com.project15.server.member;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry
+                .addMapping("/*")
+                .allowedOrigins("")
+                .allowedMethods("*");
+    }
+}
