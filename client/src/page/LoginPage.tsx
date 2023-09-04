39700acc6e4ad4e3e24cdfc55e651856e4e361a7
Dr-eong <rwg3465@gmail.com>
2023-09-04 14:31:15 +0900

    [FE] feat: #46 로그인 페이지 구현

---
+++ client/src/page/LoginPage.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import LoginForm from "../components/LoginForm";
+import {
+  LoginPageContainer,
+  LoginPageImage,
+} from "./page_style/LoginPage_style";
+
+const LoginPage: React.FC = () => {
+  return (
+    <LoginPageContainer>
+      <LoginPageImage
+        src="https://i.pinimg.com/564x/dd/29/62/dd2962dcc7da60635745235f7377d5f1.jpg"
+        alt="img"
+      />
+      <LoginForm />
+    </LoginPageContainer>
+  );
+};
+export default LoginPage;
