5a814a8bd160509bd4623e04e09db572b92725d6
Dr-eong <rwg3465@gmail.com>
2023-09-05 20:16:15 +0900

    [FE] feat: 텍스트 입력 컴포넌트 구현 #69

---
+++ client/src/components/components_style/InputComponent_style.tsx
@@ -0,0 +1,41 @@
+import { styled } from "styled-components";
+import globalTokens from "../../design_tokens/global.json";
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: auto;
+`;
+
+export const InputLabel = styled.label`
+  font-size: 15px;
+  margin-bottom: 8px;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  height: 46px;
+  font-size: 15px;
+  border-radius: 6px;
+  border: none;
+  background-color: ${globalTokens.Input.default.value};
+  padding-left: 14px;
+
+  & > ::placeholder {
+    font-size: 15px;
+  }
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  &:focus {
+    background: #ffffff;
+    outline: 2px solid ${globalTokens.Input.focused_border.value};
+  }
+  @media (max-width: 768px) {
+    height: 44px;
+    font-size: 14px;
+  }
+`;
