6028546c12e66af16659bb04febd66c966bcae47
Dr-eong <rwg3465@gmail.com>
2023-09-06 14:42:34 +0900

    [FE] feat: 텍스트입력 데이터 타입 설정

---
+++ client/src/type/type.tsx
@@ -0,0 +1,12 @@
+export interface InputData {
+  email: string;
+  password: string;
+  nickname: string;
+}
+
+export interface InputProps {
+  labelText?: string;
+  placeholder?: string;
+  type?: string;
+  name?: string;
+}
