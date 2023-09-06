d9abc1ec4896b750ee93b4d8f6667ee20ca5885e
Dr-eong <rwg3465@gmail.com>
2023-09-06 14:31:36 +0900

    [FE] docs: 텍스트 입력창 스타일 수정

--- client/src/components/InputComponent.tsx
+++ client/src/components/InputComponent.tsx
@@ -4,12 +4,39 @@ import {
   InputLabel,
   Input,
 } from "./components_style/InputComponent_style";
+import { InputData, InputProps } from "../type/type";
+import { useInput } from "../hooks/InputHandler";
 
-const InputComponent: React.FC = () => {
+// FIXME :useParams를 이용, 페이지마다 input이 다르게
+
+const data: InputData = {
+  email: "",
+  password: "",
+  nickname: "",
+};
+
+const InputComponent: React.FC<InputProps> = ({
+  labelText,
+  type,
+  name,
+  placeholder,
+}) => {
+  const emailInput = useInput(data.email);
+  const passwordInput = useInput(data.password);
+  const nicknameInput = useInput(data.nickname);
+
   return (
     <InputContainer>
-      <InputLabel>입력창 라벨</InputLabel>
-      <Input placeholder="자리표시자" />
+      <InputLabel>{labelText}</InputLabel>
+      <Input
+        type={type}
+        name={name ? name : type}
+        placeholder={placeholder}
+        value={type === "email" ? emailInput.value : passwordInput.value}
+        onChange={
+          type === "email" ? emailInput.onChange : passwordInput.onChange
+        }
+      />
     </InputContainer>
   );
 };
