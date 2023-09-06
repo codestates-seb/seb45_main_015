aa8ffad99420467d218069b53ad3707ccc5210b0
Dr-eong <rwg3465@gmail.com>
2023-09-06 14:30:27 +0900

    [FE] docs: 토스트 알림창 스타일 수정

--- client/src/alret/Toast.tsx
+++ client/src/alret/Toast.tsx
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import { ToastContainer } from "./alret_style/Toast_sytled";
 
 const Toast: React.FC = () => {
-  const [slide, setSilde] = useState(false);
+  const [slide, setSilde] = useState<string>("false");
   return (
     <>
-      <ToastContainer slide={slide} onClick={prev => setSilde(!prev)}>
+      <ToastContainer slide={slide} onClick={() => setSilde("true")}>
         <span className="toast toast-title">알림종류</span>
         <span className="toast toast-description">
           등록하신 물건의 유예기간이 종료되었습니다
--- client/src/alret/alret_style/Toast_sytled.tsx
+++ client/src/alret/alret_style/Toast_sytled.tsx
@@ -1,45 +1,45 @@
 import styled from "styled-components";
 import globalTokens from "../../design_tokens/global.json";
 
-export const ToastContainer = styled.div<{ slide: boolean }>`
-  width: 33rem;
-  height: 7rem;
+export const ToastContainer = styled.div<{ slide: string }>`
+  width: 330px;
+  height: 70px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   background-color: ${globalTokens.Toast_green.value};
-  gap: 1rem;
-  border-radius: 6px;
+  gap: 10px;
   color: #ffffff;
   position: fixed;
-  top: 3rem;
-  right: -33rem;
-  /* right: 0rem; */
-  animation: ${props => (props.slide ? "slideout 1s" : "slidein 1.5s")} linear;
+  top: 90px;
+  right: ${props => (props.slide === "true" ? "-330px" : "0px")};
+  animation: ${props =>
+      props.slide === "true" ? "slideout 1s" : "slidein 1.5s"}
+    linear;
   z-index: 1000;
 
   @keyframes slidein {
     0% {
-      right: -33rem;
+      right: -330px;
     }
 
     100% {
-      right: 0rem;
+      right: 0px;
     }
   }
   @keyframes slideout {
     0% {
-      right: 0rem;
+      right: 0px;
     }
 
     100% {
-      right: -33rem;
+      right: -330px;
     }
   }
 
   & > .toast {
-    font-size: 1.4rem;
-    margin-left: 1rem;
+    font-size: 14px;
+    margin-left: 10px;
   }
 
   & > .toast-title {
