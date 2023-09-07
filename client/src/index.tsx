07c90350e4888be1c11c4e5a23a8bb0b497e1fdb
Dr-eong <rwg3465@gmail.com>
2023-09-07 13:25:24 +0900

    [FE] docs: 리액트쿼리 설치, index.tsx에 쿼리클라이언트 추가

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,15 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { store } from "./store";
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
       <App />
-    </Provider>
+      <ReactQueryDevtools initialIsOpen />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
