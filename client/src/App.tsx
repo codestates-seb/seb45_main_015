56cad66145d64b36f074c29fbf60039eb66029a2
dice0314 <dice0314@naver.com>
2023-09-07 17:10:22 +0900

    feat: myPage 폼 제작

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,6 +14,7 @@ import ItemListPage from "./page/ItemListPage";
 import MyPage from "./page/MyPage";
 // import ItemCategoryPage from "./page/ItemCategoryPage";
 // import FavoritePage from "./page/FavoritePage";
+import MyPage from "./page/MyPage";
 
 const App: React.FC = () => {
   return (
@@ -34,6 +35,7 @@ const App: React.FC = () => {
             <Route path="/mypage" element={<MyPage />} />
             {/* <Route path="/category" element={<ItemCategoryPage />} />
             <Route path="/favorite" element={<FavoritePage />} /> */}
+            <Route path="/mypage" element={<MyPage />} />
           </Routes>
         </div>
         <Footer />
