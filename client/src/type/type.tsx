85d81affe03fcd2b19103a4821e689f57f87cf66
Dr-eong <rwg3465@gmail.com>
2023-09-12 10:03:14 +0900

    [FE] feat: 사용자입력 정보 타입 추가

--- client/src/type/type.tsx
+++ client/src/type/type.tsx
@@ -1,12 +1,28 @@
-export interface InputData {
+import { Dispatch, SetStateAction } from "react";
+
+export type SignupData = {
   email: string;
   password: string;
   nickname: string;
-}
+};
 
-export interface InputProps {
-  labelText?: string;
-  placeholder?: string;
-  type?: string;
-  name?: string;
+// 로그인 데이터
+export type LoginData = Omit<SignupData, "nickname">;
+
+// 비밀번호 변경 - 인증메일
+export type FindPWData = Pick<SignupData, "email">;
+
+// 비밀번호 변경데이터 - 비밀번호입력
+export type ChangePWData = Pick<SignupData, "password">;
+
+export interface InputField {
+  type: string;
+  name: string;
+  labelText: string;
+  placeholder: string;
+  stateValue?: SignupData | LoginData | FindPWData | ChangePWData;
+  errorMessage?: string;
+  setStateValue: Dispatch<
+    SetStateAction<SignupData | LoginData | FindPWData | ChangePWData>
+  >;
 }
