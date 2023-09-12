b503baaec35988728c85b4d91310eb1bbb572fa8
Dr-eong <rwg3465@gmail.com>
2023-09-12 10:08:48 +0900

    [FE] feat: 버튼 검포넌트 추가, 비밀번호 변경 핸들러 추가

--- client/src/page/ChangePasswordPage.tsx
+++ client/src/page/ChangePasswordPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import {
   ChangePasswordPageContainer,
   ChangePasswordPageImage,
@@ -7,41 +7,56 @@ import {
   ChangePasswordFormItem,
   ChangePasswordFormContainer,
 } from "./page_style/ChangePasswordPage_style";
-// import {} from "../API/FetchAPI";
+import { useChange } from "../API/FetchAPI";
 import InputComponent from "../components/InputComponent";
+import { LargeButtonB } from "../components/ButtonComponent";
+import { ChangePWData } from "../type/type";
 
 const ChangePasswordPage: React.FC = () => {
+  const navigator = useNavigate();
+  const [userInfo, setUserInfo] = useState<ChangePWData>({
+    password: "",
+  });
+  const [passwordMessage, setPasswordMessage] = useState("");
+  const [isPassword, setIsPassword] = useState(false);
+
+  const handleChangePWSumbit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    useChange(userInfo);
+  };
+
   return (
     <ChangePasswordPageContainer>
       <ChangePasswordPageImage />
       <ChangePasswordFormDiv>
-        <ChangePasswordFormContainer>
+        <ChangePasswordFormContainer onSubmit={handleChangePWSumbit}>
           <ChangePasswordFormItem>
             <h2>비밀번호 변경</h2>
             <InputComponent
-              labelText="새 비밀번호"
               type="password"
               name="password"
+              labelText="새로운 비밀번호"
               placeholder="새로운 비밀번호를 입력해주세요"
+              stateValue={userInfo}
+              setStateValue={setUserInfo}
+              errorMessage={passwordMessage}
             />
             <InputComponent
-              labelText="비밀번호확인"
               type="password"
               name="password"
-              placeholder="위와 같은 비밀번호를 입력해주세요"
+              labelText="비밀번호 확인"
+              placeholder="다시한번 비밀번호를 입력해주세요"
+              stateValue={userInfo}
+              setStateValue={setUserInfo}
+              errorMessage={passwordMessage}
             />
             <span className="password-convention">
               특수문자, 소문자, 숫자를 조합하여 8자리 이상 16자리 이하로
               입력해주세요.
             </span>
-            <button
-              className="change-btn"
-              onClick={e => {
-                e.preventDefault();
-              }}
-            >
-              변경하기
-            </button>
+            <div className="button-container">
+              <LargeButtonB value="변경하기" />
+            </div>
           </ChangePasswordFormItem>
         </ChangePasswordFormContainer>
       </ChangePasswordFormDiv>
