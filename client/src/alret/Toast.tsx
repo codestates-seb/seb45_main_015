86a2f170115bdd1abb11c52b5cbd86304a47143c
Dr-eong <rwg3465@gmail.com>
2023-09-17 21:22:49 +0900

    [FE] fix: type오류 수정

--- client/src/alret/Toast.tsx
+++ client/src/alret/Toast.tsx
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import { ToastContainer } from "./alret_style/Toast_sytled";
 
 const Toast: React.FC = () => {
-  const [slide, setSilde] = useState<string>("false");
+  const [slide, setSilde] = useState<boolean>(false);
   return (
     <>
-      <ToastContainer slide={slide} onClick={() => setSilde("true")}>
-        <span className="toast toast-title">알림종류</span>
+      <ToastContainer slide={slide.toString()} onClick={() => setSilde(true)}>
+        <span className="toast toast-title">{"알림종류"}</span>
         <span className="toast toast-description">
           등록하신 물건의 유예기간이 종료되었습니다
         </span>
