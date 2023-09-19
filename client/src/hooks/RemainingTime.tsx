ec98b76e9df8e4bf517f7e202900cf09ff542f4f
dice0314 <dice0314@naver.com>
2023-09-19 09:55:33 +0900

    refactor: 남은시간 함수 분리

---
+++ client/src/hooks/RemainingTime.tsx
@@ -0,0 +1,29 @@
+import dayjs from "dayjs";
+
+export function RemainingTime(endTime: string | undefined) {
+  const now = dayjs();
+
+  if (endTime) {
+    const targetTime = dayjs(endTime);
+    const timeDifference = targetTime.diff(now, "second");
+
+    const daysRemaining = Math.floor(timeDifference / (60 * 60 * 24));
+    const hoursRemaining = Math.floor(
+      (timeDifference % (60 * 60 * 24)) / (60 * 60),
+    );
+    const minutesRemaining = Math.floor((timeDifference % (60 * 60)) / 60);
+    const secondsRemaining = timeDifference % 60;
+
+    if (
+      daysRemaining < 0 &&
+      hoursRemaining < 0 &&
+      minutesRemaining < 0 &&
+      secondsRemaining < 0
+    ) {
+      return "해당 상품의 경매가 종료되었습니다.";
+    }
+
+    return `${daysRemaining}일 ${hoursRemaining}시간 ${minutesRemaining}분 ${secondsRemaining}초`;
+  }
+  return "남은 시간을 불러오는데 실패하였습니다.";
+}
