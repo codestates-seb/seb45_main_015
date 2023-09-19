4d35ab9ede51ceab4ded53abc81831f10b48d1c5
leesaewoo <ljww123@naver.com>
2023-09-19 15:50:06 +0900

    fix: 입찰 후 캐시 업데이트

--- server/src/main/java/com/project15/server/bid/service/BidService.java
+++ server/src/main/java/com/project15/server/bid/service/BidService.java
@@ -1,6 +1,7 @@
 package com.project15.server.bid.service;
 
 import com.project15.server.bid.entity.Bid;
+import com.project15.server.item.dto.ItemDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,5 +9,5 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public interface BidService {
 
-    void createBid(Bid bid);
+    ItemDto.ResponseDto createBid(Bid bid, Long itemId);
 }
