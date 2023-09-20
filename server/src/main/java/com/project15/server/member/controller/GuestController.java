50929766a3f147da16ba904da78395a448f40a1f
rlawogks <otfine@gmail.com>
2023-09-20 11:44:31 +0900

    fix: 게스트로그인 수정

--- server/src/main/java/com/project15/server/member/controller/GuestController.java
+++ server/src/main/java/com/project15/server/member/controller/GuestController.java
@@ -1,7 +1,10 @@
 package com.project15.server.member.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project15.server.member.repository.AuthService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,17 +12,31 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/members")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class GuestController {
 
-    @Autowired
-    private AuthService authService;
+    private final AuthService authService;
 
     @PostMapping("/guest-login")
     public ResponseEntity<String> guestLogin() {
         String token = authService.guestLogin();
-        return new ResponseEntity<>(token, HttpStatus.OK);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonResponse;
+        try {
+            jsonResponse = objectMapper.writeValueAsString(Collections.singletonMap("token", token));
+        } catch (JsonProcessingException e) {
+            return new ResponseEntity<>("Failed to serialize JSON response", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+
+        return new ResponseEntity<>(jsonResponse, httpHeaders, HttpStatus.OK);
     }
 }
