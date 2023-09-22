baa3a9bc770a95e7842276f465daa87f276636aa
rlawogks <otfine@gmail.com>
2023-09-22 13:36:56 +0900

    fix : 게스트로그인 수정

--- server/src/main/java/com/project15/server/member/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project15.server.member.repository;
-
-import com.project15.server.member.entity.GuestUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<GuestUser, Long> {
-
-}
